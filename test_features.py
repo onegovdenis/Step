707c195a369f83d97cc5c9c5b4e59588828acfb1
Denis <onegov.d@gmail.com>
2020-06-14 16:28:04 +0300

    test money

---
+++ test_features.py
@@ -0,0 +1,14 @@
+import unittest
+from features import *
+
+
+class TestPin(unittest.TestCase):
+    def setUp(self):
+       self.my_atm = Atm()
+
+
+    def test_money(self):
+        self.assertTrue(self.my_atm.rise_money(1000))
+
+
+
